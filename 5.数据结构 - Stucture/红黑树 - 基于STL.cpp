b913eaf564e8c198108327a22b9c537d290098c9
DCTewi <dctewi@dctewi.com>
2019-05-01 17:23:41 +0800

    Update 红黑树 - 基于STL.cpp
    一些微小的工作

--- 5.数据结构 - Stucture/红黑树 - 基于STL.cpp
+++ 5.数据结构 - Stucture/红黑树 - 基于STL.cpp
@@ -10,6 +10,8 @@ using namespace __gnu_pbds;
 typedef long long ll;
 typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> rbtree;
 
+const int Offset = 20;
+
 rbtree tr;
 
 int main()
@@ -23,37 +25,37 @@ int main()
             case 1:
             {
                 // Add
-                tr.insert((x << 20) + i);
+                tr.insert((x << Offset) + i);
             }
             break;
             case 2:
             {
                 // Del
-                tr.erase(tr.lower_bound(x << 20));
+                tr.erase(tr.lower_bound(x << Offset));
             }
             break;
             case 3:
             {
                 // Get rank
-                cout << tr.order_of_key(x << 20) + 1 << "\n";
+                cout << tr.order_of_key(x << Offset) + 1 << "\n";
             }
             break;
             case 4:
             {
                 //Get kth
-                cout << (*tr.find_by_order(x - 1) >> 20) << "\n";
+                cout << (*tr.find_by_order(x - 1) >> Offset) << "\n";
             }
             break;
             case 5:
             {
                 // Get Prev
-                cout << (*--tr.lower_bound(x << 20) >> 20) << "\n";
+                cout << (*--tr.lower_bound(x << Offset) >> Offset) << "\n";
             }
             break;
             case 6:
             {
                 // Get Next
-                cout << (*tr.lower_bound((x + 1) << 20) >> 20) << "\n";
+                cout << (*tr.lower_bound((x + 1) << Offset) >> Offset) << "\n";
             }
             break;
             default: break;
